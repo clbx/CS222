5528809906d6aeb6971007867371b82309a1eb8d
Clay Buxton <clay@clbx.io>
2018-04-08 18:19:41 -0400

    Working on Project 5

--- Projects/Project 5/potter.c
+++ Projects/Project 5/potter.c
@@ -4,93 +4,166 @@
 #include <string.h>
 
 
-typedef struct node
-{
-  struct node* left;
-  struct node* right;
-  int data;
-} node;
-
-
-node* newNode(int data);
-node* insert(node* branch, int data);
-void print(node* branch);
-node* search(node* branch, int data);
-node* remove(node* branch, int data);
+Student* newStudent(char* first, char* last, int points, int year, House house);
+Student* insert(Student* root, Student* node);
+void print(Student* root);
+Student* search(Student* root, char* first, char* last);
+Student* removeStudent(Student* root, char* first, char* last);
+void printstudent(Student* root);
+char* addNames(char* first, char* last);
+Student* findSmallest(Student* root);
 
 int main(int argc, char** argv)
 {
-  node* root = (node*)malloc(sizeof(node));
-  root->left = NULL;
-  root->right = NULL;
-  root->data = 25;
+  //CREATE HOUSES
+  Student** GryfPoint;
+  Student** RavePoint;
+  Student** HuffPoint;
+  Student** SlytPoint;
+
+  Student* Gryf = (Student*)malloc(sizeof(Student));
+  Student* Rave = (Student*)malloc(sizeof(Student));
+  Student* Huff = (Student*)malloc(sizeof(Student));
+  Student* Slyt = (Student*)malloc(sizeof(Student));
+
+  Gryf = NULL;
+  Rave = NULL;
+  Huff = NULL;
+  Slyt = NULL;
+
+  GryfPoint = &Gryf;
+  RavePoint = &Rave;
+  HuffPoint = &Huff;
+  SlytPoint = &Slyt;
+
+  Student* Harry = newStudent("Harry", "Potter", 50, 3, GRYFFINDOR);
+  *GryfPoint = insert(*GryfPoint, Harry);
+  Student* Ron = newStudent("Ron", "Weasley", 150, 3, GRYFFINDOR);
+  *GryfPoint = insert(*GryfPoint, Ron);
+  Student* Hermione = newStudent("Hermione", "Granger", 500, 3, GRYFFINDOR);
+  *GryfPoint = insert(*GryfPoint, Hermione);
+  printf("=============\n");
+  print(*GryfPoint);
+  printf("=============\n");
+  printstudent(search(*GryfPoint, "Ron", "Weasley"));
+  *GryfPoint = removeStudent(*GryfPoint, "Harry", "Potter");
+  printf("=============\n");
+  print(*GryfPoint);
 
 
-
-  insert(root, 10);
-  insert(root, 20);
-  insert(root, 60);
-  insert(root, 12);
-  insert(root, 50);
-  insert(root, 5);
-
-  print(root);
-
-  int lookFor = 1243;
-
-  if(search(root, lookFor) != NULL)
-    printf("%d", search(root, lookFor)->data);
-  else
-    printf("%d", 0);
-
-  printf("\n");
 }
 
-void print(node* branch)
+Student* insert(Student* root, Student* node)
 {
-  if(branch != NULL)
+  fprintf(stderr, "Gets inside\n");
+  if(root == NULL)
   {
-    print(branch->left);
-    printf("%d ", branch->data);
-    print(branch->right);
+    return node;
   }
 
+  if(strcmp(root->last, node->last) > 0)
+    root->left = insert(root->left, node);
+  else if(strcmp(root->last, node->last) < 0)
+    root->right = insert(root->right, node);
 }
 
-node* insert(node* branch,int data)
+Student* search(Student* root, char* first, char* last)
 {
-  if(branch == NULL)
-    return newNode(data);
-  if(data < branch->data)
-    branch->left = insert(branch->left, data);
-  else if(data > branch->data)
-    branch->right = insert(branch->right, data);
-}
-
-node* search(node* branch, int data)
-{
-  if(branch == NULL)
-  {
+  char* name = addNames(first, last);
+  char* rootName = addNames(root->first, root->last);
+  if(root == NULL)
     return NULL;
+  else if(strcmp(name, rootName)==0)
+	{
+     return root;
+	}
+  else if(strcmp(name, rootName)<0)
+	{
+		return search(root->left, first, last);
+	}
+	else
+	{
+		return search(root->right, first, last);
+	}
+
+}
+
+char* addNames(char* first, char* last)
+{
+  char* name = (char*)malloc(sizeof(char));
+  name = strcat(name, last);
+  name = strcat(name, first);
+  return name;
+}
+
+void print(Student* root)
+{
+  if(root != NULL)
+  {
+    print(root->left);
+    printstudent(root);
+    print(root->right);
   }
-  else if(branch->data == data)
-    return branch;
-  else if(data<branch->data)
-    return search(branch->left, data);
+}
+
+void printstudent(Student* root)
+{
+  printf("%s %s (Year: %d) - Points: %d, House %s\n", root->first, root->last, root->year, root->points, HOUSE_NAMES[root->house]);
+}
+
+Student* newStudent(char* first, char* last, int points, int year, House house)
+{
+  Student* student = (Student*)malloc(sizeof(Student));
+  student->first = strdup(first);
+  student->last = strdup(last);
+  student->points = points;
+  student->year = year;
+  student->house = house;
+  student->left = NULL;
+  student->right = NULL;
+  return student;
+}
+
+Student* removeStudent(Student* root, char* first, char* last)
+{
+  char* name = addNames(first, last);
+  char* rootName = addNames(root->first, root->last);
+  if(root == NULL)
+    return root;
+  if(strcmp(name, rootName) < 0)
+    root->left = removeStudent(root->left, first, last);
+  else if(strcmp(name, rootName) > 0)
+    root->right = removeStudent(root->right, first, last);
   else
-    return search(branch->right, data);
+  {
+    if(root->left == NULL)
+    {
+      Student* temp = root->right;
+      free(root);
+      return temp;
+    }
+    else if(root->right == NULL)
+    {
+      Student* temp = root->left;
+      free(root);
+      return temp;
+    }
+    Student* temp = findSmallest(root->right);
+    root->first = temp->first;
+    root->last = temp->last;
+    root->right = removeStudent(root->right, first, last);
+  }
+  return root;
+
 }
 
-node* remove(node* branch, int data)
+Student* findSmallest(Student* root)
 {
-  
+  Student* current = root;
+  while(current->left != NULL)
+    current = current -> left;
+
+  return current;
 }
 
-node* newNode(int data)
-{
-  node* branch = (node*)malloc(sizeof(node));
-  branch->data = data;
-  branch->left = NULL;
-  branch->right = NULL;
-  return branch;
-}
+//hello
